8bc5076e8d9b4f23826eb1e597911d0c3542958c
Jacqueline Remmel, Lauren Musante and sofia giordano <sofia@>
2019-09-26 11:18:45 -0700

    add more branching to catch user errors, and add recursive method call

--- program.cs
+++ program.cs
@@ -10,24 +10,45 @@ class Program
     Plant myPlant = new Plant();
     //here we are using myPlant as our plantrameter. We are calling the below method now, in the main. We created showPlantStats to dry up the code...prevents us from having to repeat "Console.WriteLine" every time we want to show the plant stats.
     ShowPlantStats(myPlant);
-    Console.WriteLine("To water your plant, press 'w'; to sun your plant, press 's'");
-    string userAnswer = Console.ReadLine();
-    if (userAnswer.ToLower() == "w" || userAnswer.ToLower() == "water")
-    {
-        myPlant.Water();
-        Console.WriteLine("You watered your plant! Your plant's new height is " + myPlant.Height);
-    }
-    else if (userAnswer.ToLower() == "s" || userAnswer.ToLower() == "sun")
-    {
-        string health = myPlant.GiveSunshine();
-        Console.WriteLine("You sunned your plant! Your plant's new health is " + health);
-    }
+    PromptUser(myPlant);
+
   }
   //"Plant" is telling the method what data type the argument will be. 
   public static void ShowPlantStats(Plant plantrameter)
   {
-    Console.WriteLine("Your plant's health level: " + plantrameter.Health);
+    Console.WriteLine("Your plant's health level: " + plantrameter.HealthArray[plantrameter.Health]);
     Console.WriteLine("Your plant's height: " + plantrameter.Height);
     Console.WriteLine("Your plant has " + plantrameter.Flowers + " flowers.");
   }
+  
+//   this method has a new parameter of paramaplant, different than plantrameter because its a new method. however both are plant objects. when we call each of these method both will take in the argument myPlant.
+  public static void PromptUser(Plant paramaPlant)
+{
+    Console.WriteLine("To water your plant, press 'w'; to sun your plant, press 's', to show your plant's description, press 'd'");
+    string userAnswer = Console.ReadLine();
+    
+    if (userAnswer.ToLower() == "w" || userAnswer.ToLower() == "water")
+    {
+        paramaPlant.Water();
+        Console.WriteLine("You watered your plant! Your plant's new height is " + paramaPlant.Height);
+        PromptUser(paramaPlant);
+    }
+    else if (userAnswer.ToLower() == "s" || userAnswer.ToLower() == "sun")
+    {
+        string health = paramaPlant.GiveSunshine();
+        Console.WriteLine("You sunned your plant! Your plant's new health is " + health);
+        PromptUser(paramaPlant);
+    }
+    else if (userAnswer.ToLower() == "d" || userAnswer.ToLower() == "description")
+    {
+        ShowPlantStats(paramaPlant);
+        PromptUser(paramaPlant);
+    }
+    else 
+    {
+        Console.WriteLine("Nice try! Go plant yourself!");
+        PromptUser(paramaPlant);
+    }
+}
+
 }
